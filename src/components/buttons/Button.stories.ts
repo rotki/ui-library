df125d9964f8fbaccf1a3be3c4a5bf0bc2add993
Joseph Ojoko <entewshi@gmail.com>
2023-05-02 12:40:25 +0100

    feat: update button story

--- src/components/buttons/Button.stories.ts
+++ src/components/buttons/Button.stories.ts
@@ -1,4 +1,4 @@
-import { type StoryFn } from '@storybook/vue';
+import { type Meta, type StoryFn } from '@storybook/vue';
 import Button from './Button.vue';
 
 const render: StoryFn<typeof Button> = (_, { argTypes }) => ({
@@ -7,26 +7,145 @@ const render: StoryFn<typeof Button> = (_, { argTypes }) => ({
   template: '<Button v-bind="$props" />'
 });
 
-export default {
-  title: 'Example/Button',
+const meta: Meta<typeof Button> = {
+  title: 'Components/Button',
   component: Button,
   tags: ['autodocs'],
   render,
   argTypes: {
-    color: { control: 'color' },
-    outlined: { control: 'boolean' }
+    label: { control: 'text' },
+    outlined: { control: 'boolean', table: { category: 'Style' } },
+    secondary: { control: 'boolean', table: { category: 'Color' } },
+    error: { control: 'boolean', table: { category: 'Color' } },
+    tile: { control: 'boolean', table: { category: 'Shape' } },
+    elevated: { control: 'boolean', table: { category: 'Shape' } },
+    text: { control: 'boolean', table: { category: 'Style' } },
+    sm: { control: 'boolean', table: { category: 'Size' } },
+    lg: { control: 'boolean', table: { category: 'Size' } },
+    disabled: { control: 'boolean', table: { category: 'State' } },
+    loading: { control: 'boolean', table: { category: 'State' } }
+  },
+  parameters: {
+    docs: {
+      controls: { exclude: ['prefix', 'suffix'] }
+    }
   }
 };
 
 export const Primary = {
   args: {
-    label: 'Button'
+    label: 'Medium'
   }
 };
 
-export const Outlined = {
+export const PrimaryText = {
+  args: {
+    label: 'Large',
+    text: true,
+    lg: true
+  }
+};
+
+export const PrimarySquare = {
+  args: {
+    label: 'Medium',
+    tile: true
+  }
+};
+
+export const PrimarySmall = {
+  args: {
+    label: 'Small',
+    sm: true
+  }
+};
+
+export const PrimaryLarge = {
+  args: {
+    label: 'Large',
+    lg: true,
+    elevated: true
+  }
+};
+
+export const PrimaryLargeSquare = {
+  args: {
+    label: 'Large',
+    tile: true,
+    lg: true,
+    elevated: true
+  }
+};
+
+export const PrimaryDisabled = {
+  args: {
+    label: 'Medium',
+    disabled: true
+  }
+};
+
+export const PrimaryOutlined = {
   args: {
     label: 'Outlined Button',
     outlined: true
   }
 };
+
+export const Secondary = {
+  args: {
+    label: 'Secondary Button',
+    secondary: true
+  }
+};
+
+export const SecondaryText = {
+  args: {
+    label: 'Secondary Button',
+    secondary: true,
+    text: true,
+    lg: true
+  }
+};
+
+export const SecondaryOutlined = {
+  args: {
+    label: 'Outlined Button',
+    secondary: true,
+    outlined: true
+  }
+};
+
+export const ErrorButton = {
+  args: {
+    label: 'Error Button',
+    error: true
+  }
+};
+
+export const ErrorButtonText = {
+  args: {
+    label: 'Error Button',
+    error: true,
+    text: true,
+    lg: true
+  }
+};
+
+export const ErrorOutlined = {
+  args: {
+    label: 'Error Button',
+    error: true,
+    outlined: true
+  }
+};
+
+export const ErrorOutlinedDisabled = {
+  args: {
+    label: 'Error Button',
+    error: true,
+    outlined: true,
+    disabled: true
+  }
+};
+
+export default meta;
