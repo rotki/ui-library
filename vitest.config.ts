3112222f3f24e87278d4c91ce5feeb231e749473
Joseph Ojoko <entewshi@gmail.com>
2023-06-01 17:21:15 +0100

    chore: add vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,25 @@
+import { resolve } from 'node:path';
+import { mergeConfig } from 'vite';
+import { configDefaults, defineConfig } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude],
+      root: resolve(__dirname),
+      reporters: ['default', 'html', 'json'],
+      outputFile: 'tests/html/index.html',
+      coverage: {
+        provider: 'c8',
+        reportsDirectory: 'tests/coverage',
+        reporter: ['html', 'json'],
+        include: ['src/*'],
+        exclude: ['node_modules', 'tests/', '**/*.d.ts'],
+      },
+    },
+  })
+);
