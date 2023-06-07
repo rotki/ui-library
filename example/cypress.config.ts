aeb538964c52dd802f72faf15b92e60d7cb05f9d
Konstantinos Paparas <kelsos86@gmail.com>
2023-06-07 21:07:32 +0200

    ci: enables cypress tests on ci

--- example/cypress.config.ts
+++ example/cypress.config.ts
@@ -1,3 +1,4 @@
+import * as fs from 'node:fs';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
@@ -6,5 +7,23 @@ export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.ts',
     baseUrl: 'http://localhost:4173',
+    setupNodeEvents: (on): void => {
+      on(
+        'after:spec',
+        (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+          if (results && results.video) {
+            // Do we have failures for any retry attempts?
+            const failures = results.tests.some((test) =>
+              test.attempts.some((attempt) => attempt.state === 'failed')
+            );
+            if (!failures) {
+              // delete the video if the spec passed and no tests retried
+
+              fs.unlinkSync(results.video);
+            }
+          }
+        }
+      );
+    },
   },
 });
