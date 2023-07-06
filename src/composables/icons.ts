2fd6bbfc82354416cfe1023c75b6c51ecfb148ce
Joseph Ojoko <entewshi@gmail.com>
2023-07-06 01:11:22 +0100

    feat: register used stepper icons

--- src/composables/icons.ts
+++ src/composables/icons.ts
@@ -1,15 +1,25 @@
 import { type GeneratedIcon } from '@/types/icons';
 import {
+  RiAlertFill,
+  RiCheckLine,
   RiCheckboxBlankLine,
+  RiCheckboxCircleFill,
   RiCheckboxFill,
   RiCheckboxIndeterminateFill,
-} from '../all-icons';
+  RiErrorWarningFill,
+  RiInformationFill,
+} from '@/all-icons';
 
 export const useIcons = createGlobalState(() => {
   const requiredIcons: GeneratedIcon[] = [
     RiCheckboxFill,
+    RiCheckboxCircleFill,
     RiCheckboxIndeterminateFill,
     RiCheckboxBlankLine,
+    RiCheckLine,
+    RiErrorWarningFill,
+    RiAlertFill,
+    RiInformationFill,
   ];
 
   const registeredIcons: Ref<Record<string, string>> = ref({});
