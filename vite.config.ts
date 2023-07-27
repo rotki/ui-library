5e66cdf65d4f80cbe0d2361d61f7870433d37fee
Konstantinos Paparas <kelsos86@gmail.com>
2023-07-27 16:43:19 +0200

    fix: adds typeVersions to fix subpath import errors

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,25 @@ import { resolve } from 'node:path';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vitest/config';
 import AutoImport from 'unplugin-auto-import/vite';
+import fg from 'fast-glob';
+
+const entryPoints = [
+  'src/components/index.ts',
+  'src/composables/index.ts',
+  'src/all-icons.ts',
+  'src/theme/index.ts',
+  'src/index.ts',
+];
+
+const files = fg.sync(entryPoints, { absolute: true });
+
+const entities = files.map((file) => {
+  const [key] = file.match(/(?<=src\/).*$/) || [];
+  const keyWithoutExt = key.replace(/\.[^.]*$/, '');
+  return [keyWithoutExt, file];
+});
+
+const entries = Object.fromEntries(entities);
 
 export default defineConfig({
   resolve: {
@@ -25,17 +44,12 @@ export default defineConfig({
   build: {
     outDir: './dist',
     lib: {
-      entry: {
-        index: resolve(__dirname, 'src/index.ts'),
-        theme: resolve(__dirname, 'src/theme/index.ts'),
-        components: resolve(__dirname, 'src/components/index.ts'),
-        composables: resolve(__dirname, 'src/composables/index.ts'),
-      },
+      entry: entries,
       fileName: (format, entryName) => `${entryName}.${format}.js`,
       formats: ['es'],
     },
     rollupOptions: {
-      external: ['vue', 'tailwindcss/plugin'],
+      external: ['vue', 'tailwindcss/plugin', '@vueuse/core', '@vueuse/shared'],
       output: {
         globals: {
           vue: 'vue',
