09afa6439aeb60bf6e728657c5e9b0c8d4ee2f2f
Joseph Ojoko <entewshi@gmail.com>
2023-11-29 20:43:54 +0100

    feat(Divider): add divider component

---
+++ src/components/divider/Divider.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+export interface Props {
+  vertical?: boolean;
+}
+
+defineOptions({
+  name: 'RuiDivider',
+});
+
+withDefaults(defineProps<Props>(), {
+  vertical: false,
+});
+</script>
+
+<template>
+  <div :class="vertical ? 'border-l' : 'border-t'" class="border-default" />
+</template>
