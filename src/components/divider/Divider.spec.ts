fa6b0c9066215aebace95056a3442e3a0a3ebe1c
Joseph Ojoko <entewshi@gmail.com>
2023-12-06 20:50:28 +0100

    feat: add divider component

---
+++ src/components/divider/Divider.spec.ts
@@ -0,0 +1,25 @@
+import { describe, expect, it } from 'vitest';
+import { mount } from '@vue/test-utils';
+import Divider from './Divider.vue';
+
+const createWrapper = (options?: any) =>
+  mount(Divider, { ...options, stubs: { RuiIcon: true } });
+
+describe('Divider', () => {
+  it('renders properly', () => {
+    const wrapper = createWrapper();
+    expect(wrapper.find('div')).toBeTruthy();
+  });
+
+  it('passes vertical props', async () => {
+    const wrapper = createWrapper();
+    expect(wrapper.get('div').classes()).not.toMatch(/border-l/);
+    expect(wrapper.get('div').classes()).toMatch(/border-t/);
+    await wrapper.setProps({ vertical: true });
+    expect(wrapper.get('div').classes()).not.toMatch(/border-t/);
+    expect(wrapper.get('div').classes()).toMatch(/border-l/);
+    await wrapper.setProps({ vertical: false });
+    expect(wrapper.get('div').classes()).not.toMatch(/border-l/);
+    expect(wrapper.get('div').classes()).toMatch(/border-t/);
+  });
+});
