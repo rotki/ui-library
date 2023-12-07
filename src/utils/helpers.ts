3a6b2589fcea876c06f1cc2b1563a40b95e208cc
Joseph Ojoko <entewshi@gmail.com>
2023-12-07 15:54:26 +0100

    chore: add function that returns root and main attrs

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,9 +1,16 @@
+import { type SetupContext } from 'vue';
+import { objectOmit, objectPick } from '@vueuse/shared';
+
 interface CurrencyOptions {
   currency?: string;
   locale?: string;
   style?: string;
   fractionDigits?: number;
 }
+
+type SetupContextAttrs = SetupContext['attrs'];
+
+type SetupContextAttrsKeys = (keyof SetupContextAttrs)[];
 
 /**
  * Format number value to specified currency format
@@ -46,3 +53,35 @@ export const formatNumber = (
  */
 export const formatInteger = (amount: number | string) =>
   formatNumber(amount, { fractionDigits: 0 });
+
+/**
+ * Generates and returns the array of root allowed attributes
+ * @param {SetupContextAttrs} data
+ * @returns {SetupContextAttrsKeys}
+ */
+export const getRootKeys = (data: SetupContextAttrs) =>
+  Object.keys(data).filter((key) =>
+    key.startsWith('data-'),
+  ) as SetupContextAttrsKeys;
+
+/**
+ * Picks only required attributes for root element
+ * @param {SetupContextAttrs} data
+ * @param {SetupContextAttrsKeys} include
+ * @returns {Pick<SetupContextAttrs, any>}
+ */
+export const getRootAttrs = (
+  data: SetupContextAttrs,
+  include: SetupContextAttrsKeys = ['class'],
+) => objectPick(data, [...getRootKeys(data), ...include]);
+
+/**
+ * Omits root attributes from component's attributes
+ * @param {SetupContextAttrs} data
+ * @param {SetupContextAttrsKeys} exclude
+ * @returns {Omit<SetupContextAttrs, any>}
+ */
+export const getNonRootAttrs = (
+  data: SetupContextAttrs,
+  exclude: SetupContextAttrsKeys = ['class'],
+) => objectOmit(data, [...getRootKeys(data), ...exclude]);
