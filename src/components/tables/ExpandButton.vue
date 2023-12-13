f6e22dc0c8081565a355c560cb6b20bb5f4ea656
Joseph Ojoko <entewshi@gmail.com>
2023-12-13 22:48:28 +0100

    chore: add expandable table examples

---
+++ src/components/tables/ExpandButton.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+import Button from '@/components/buttons/button/Button.vue';
+import Icon from '@/components/icons/Icon.vue';
+import { type RuiIcons } from '~/src';
+
+export interface Props {
+  expanded: boolean;
+  icon?: RuiIcons;
+}
+
+withDefaults(defineProps<Props>(), { icon: 'arrow-down-s-line' });
+
+const css = useCssModule();
+</script>
+
+<template>
+  <Button
+    :class="[
+      css.tr__expander_button,
+      {
+        [css.tr__expander_button_open]: expanded,
+      },
+    ]"
+    icon
+    size="sm"
+    variant="text"
+  >
+    <slot>
+      <Icon :class="css.tr__expander_button_icon" :name="icon" />
+    </slot>
+  </Button>
+</template>
+
+<style lang="scss" module>
+.tr__expander {
+  &_button {
+    @apply relative;
+
+    &_icon {
+      @apply transition rotate-0;
+    }
+
+    &_open .tr__expander_button_icon {
+      @apply rotate-180;
+    }
+  }
+}
+</style>
