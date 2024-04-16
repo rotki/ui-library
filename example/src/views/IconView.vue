7dddbe61c70a2d657f3c483c3aab7db41bd53c6a
Joseph Ojoko <entewshi@gmail.com>
2024-04-16 10:19:11 +0100

    fix(Logo): handles data load on ssr (#210)

--- example/src/views/IconView.vue
+++ example/src/views/IconView.vue
@@ -1,10 +1,19 @@
 <script lang="ts" setup>
-import { RuiIcon } from '@rotki/ui-library';
+import { type LogoProps, RuiIcon, RuiLogo } from '@rotki/ui-library';
 
 const icons = ref([
   { name: 'moon-line', size: 24 },
   { name: 'sun-line', size: 24 },
   { name: 'arrow-right-line', size: 24 },
+]);
+
+const logos = ref<(LogoProps & { name: string })[]>([
+  { name: 'Default' },
+  { name: 'Custom', logo: 'drawer' },
+  { name: 'Custom failed fallback to default', logo: 'nonExistingKey' },
+  { name: 'Custom failed fallback to default with app name', logo: 'nonExistingKey', text: true },
+  { name: 'Default with app name', text: true },
+  { name: 'Custom with app name', logo: 'emptyScreen', text: true },
 ]);
 </script>
 
@@ -22,6 +31,25 @@ const icons = ref([
         :key="i"
         v-bind="icon"
       />
+    </div>
+    <h2
+      class="text-h4 mb-6"
+      data-cy="logos"
+    >
+      rotki Logo
+    </h2>
+    <div class="grid gap-4 grid-rows-2 grid-cols-3">
+      <div
+        v-for="(logo, i) in logos"
+        :key="i"
+      >
+        <RuiLogo
+          v-bind="logo"
+        />
+        <div class="mt-4">
+          {{ logo.name }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- src/components/logos/Logo.vue
+++ src/components/logos/Logo.vue
@@ -1,49 +1,140 @@
 <script setup lang="ts">
-import logo from './logo.svg';
+import { useSSRContext } from 'vue';
+import { isClient } from '@vueuse/shared';
+import { transformCase } from '@/utils/helpers';
+import fallback from './logo.svg';
+
+interface ExternalLinks {
+  drawer?: string;
+  app?: string;
+  website?: string;
+  about?: string;
+  emptyScreen?: string;
+  [key: string]: string | undefined;
+}
 
 export interface Props {
   text?: boolean;
-  customSrc?: string;
+  branch?: 'develop' | 'main' | string;
+  logo?: keyof ExternalLinks;
+  size?: string | number; // in rems
+  uniqueKey?: string | number;
 }
 
 defineOptions({
   name: 'RuiLogo',
 });
 
-withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<Props>(), {
   text: false,
-  customSrc: undefined,
+  branch: 'develop',
+  logo: undefined,
+  size: 3,
+  uniqueKey: undefined,
 });
 
-const pending = ref(true);
+const customImageRef = ref<HTMLImageElement>();
 const error = ref(false);
+const decoding = ref(!!props.logo);
+const pendingSources = ref(false);
+const appName = 'rotki';
+const emptyLinks: ExternalLinks = {
+  app: undefined,
+  website: undefined,
+  about: undefined,
+  emptyScreen: undefined,
+};
+const externalSources = ref(emptyLinks);
+
+const externalSource = computed(() => {
+  const sources = get(externalSources);
+  const { logo } = props;
+  if (!logo || !sources[logo]) {
+    set(decoding, get(pendingSources));
+    return undefined;
+  }
+
+  const url = `https://raw.githubusercontent.com/rotki/data/${props.branch}/assets/icons/${sources[logo]}`;
+
+  if (props.uniqueKey !== undefined)
+    return `${url}?key=${props.uniqueKey}`;
+
+  return url;
+});
+
+async function fetchSources() {
+  if (!props.logo)
+    return set(externalSources, emptyLinks);
+
+  set(pendingSources, true);
+  try {
+    const { data } = await useFetch<string>(
+      `https://raw.githubusercontent.com/rotki/data/${props.branch}/constants/asset-mappings.json`,
+    );
+
+    set(externalSources, transformCase(JSON.parse(get(data) ?? 'null')?.logo, 'camelCase') ?? emptyLinks);
+  }
+  catch {
+    set(externalSources, emptyLinks);
+  }
+  set(pendingSources, false);
+}
+
+watch(
+  () => get(customImageRef)?.complete,
+  (complete) => {
+    if (complete) {
+      get(customImageRef)
+        ?.decode()
+        .catch(() => set(error, true))
+        .finally(() => set(decoding, false));
+    }
+  },
+);
+
+watchEffect(fetchSources);
+
+if (!isClient)
+  useSSRContext();
+
+onServerPrefetch(async () => {
+  await fetchSources();
+});
 </script>
 
 <template>
-  <div class="h-12 space-x-4 flex items-center">
-    <img
-      v-if="customSrc && !error"
-      class="h-full"
-      :class="{ hidden: pending }"
-      :src="customSrc"
-      @loadstart="pending = true"
-      @load="pending = false"
-      @error="
-        error = true;
-        pending = false;
-      "
+  <div
+    class="gap-x-4 flex items-center relative"
+    :style="{ height: `${size}rem` }"
+  >
+    <div
+      v-if="!externalSource && decoding"
+      class="h-full transition delay-0 opacity-0"
+      :style="{ width: `${size}rem` }"
     />
     <img
-      v-if="!customSrc || pending || error"
-      :src="logo"
-      alt="Rotki"
-      class="h-full"
+      v-else-if="externalSource && (!error || decoding)"
+      ref="customImageRef"
+      :src="externalSource"
+      :alt="appName"
+      data-image="custom"
+      class="h-full transition ease-out duration-200 delay-100"
+      :style="{ width: `${size}rem` }"
+      @error="error = true"
+    />
+    <img
+      v-else
+      :src="fallback"
+      :alt="appName"
+      data-image="fallback"
+      class="h-full transition ease-out duration-200 delay-100"
+      :style="{ width: `${size}rem` }"
     />
     <div
       v-if="text"
       class="text-h4 text-rui-primary dark:text-white"
     >
-      rotki
+      {{ appName }}
     </div>
   </div>
 </template>
