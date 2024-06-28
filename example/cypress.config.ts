32770d666dc42d095eef3b4bb9b4737b6cad786e
Konstantinos Paparas <kelsos86@gmail.com>
2024-06-28 12:26:40 +0200

    chore: update dependencies

--- example/cypress.config.ts
+++ example/cypress.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   viewportWidth: 1280,
   viewportHeight: 720,
+  video: true,
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.ts',
     baseUrl: 'http://localhost:4173',
