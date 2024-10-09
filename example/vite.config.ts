45d94171b2ba619862f555776203c16cbabfbb3d
Konstantinos Paparas <kelsos86@gmail.com>
2024-10-09 12:32:45 +0200

    chore: introduces custom chunk handling when packaging

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -37,4 +37,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern',
+      },
+    },
+  },
 });
