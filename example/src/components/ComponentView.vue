1b111c0dc812bd10262f93f34e4749381c6fc15c
Konstantinos Paparas <kelsos86@gmail.com>
2024-10-28 18:24:41 +0100

    refactor: change data generation for badges in the example

---
+++ example/src/components/ComponentView.vue
@@ -0,0 +1,19 @@
+<script setup lang='ts'>
+import { objectOmit, objectPick } from '@vueuse/shared';
+
+defineOptions({
+  inheritAttrs: false,
+});
+</script>
+
+<template>
+  <div v-bind="objectOmit($attrs, ['data-cy'])">
+    <h2
+      class="text-h4 mb-6"
+      v-bind="objectPick($attrs, ['data-cy'])"
+    >
+      <slot name="title" />
+    </h2>
+    <slot />
+  </div>
+</template>
