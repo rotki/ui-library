43bc2ca8ab0c4d4158b24e26d21147091b32aaeb
Konstantinos Paparas <kelsos86@gmail.com>
2025-03-10 14:28:49 +0100

    chore: update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,15 +3,15 @@ import type { StorybookConfig } from '@storybook/vue3-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-interactions',
-    '@storybook/addon-themes',
     {
       name: '@storybook/addon-essentials',
       options: {
         backgrounds: false,
       },
     },
+    '@storybook/addon-links',
+    '@storybook/addon-themes',
+    '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/vue3-vite',
