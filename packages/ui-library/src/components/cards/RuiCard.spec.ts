977678023df37f8dd027872ddfa168a152474e9c
Konstantinos Paparas <kelsos86@gmail.com>
2026-02-09 21:06:26 +0100

    test(Card): expand unit test coverage for slots and props
    Add tests for default, header, subheader, image, prepend, and footer slot
    rendering. Add tests for variant, rounded, dense, and divide prop classes.

--- packages/ui-library/src/components/cards/RuiCard.spec.ts
+++ packages/ui-library/src/components/cards/RuiCard.spec.ts
@@ -33,6 +33,118 @@ describe('components/cards/RuiCard.vue', () => {
     expect(wrapper.find('div[class*=_footer_').exists()).toBeFalsy();
   });
 
+  it('should render default slot content', () => {
+    wrapper = createWrapper({
+      slots: {
+        default: '<p>Card content</p>',
+      },
+    });
+
+    expect(wrapper.find('div[class*=_content_]').exists()).toBeTruthy();
+    expect(wrapper.find('div[class*=_content_] p').text()).toBe('Card content');
+  });
+
+  it('should render header and subheader slots', () => {
+    wrapper = createWrapper({
+      slots: {
+        header: 'Card Title',
+        subheader: 'Card Subtitle',
+      },
+    });
+
+    expect(wrapper.find('h5[class*=_header_]').exists()).toBeTruthy();
+    expect(wrapper.find('p[class*=_subheader_]').exists()).toBeTruthy();
+  });
+
+  it('should render image slot', () => {
+    wrapper = createWrapper({
+      slots: {
+        image: '<img src="test.png" alt="test" />',
+      },
+    });
+
+    expect(wrapper.find('div[class*=_image_]').exists()).toBeTruthy();
+    expect(wrapper.find('div[class*=_image_] img').exists()).toBeTruthy();
+  });
+
+  it('should render prepend slot', () => {
+    wrapper = createWrapper({
+      slots: {
+        header: 'Title',
+        prepend: '<span>Icon</span>',
+      },
+    });
+
+    expect(wrapper.find('div[class*=_prepend_]').exists()).toBeTruthy();
+  });
+
+  it('should render footer slot', () => {
+    wrapper = createWrapper({
+      slots: {
+        footer: '<button>Action</button>',
+      },
+    });
+
+    expect(wrapper.find('div[class*=_footer_]').exists()).toBeTruthy();
+    expect(wrapper.find('div[class*=_footer_] button').text()).toBe('Action');
+  });
+
+  it('should apply variant classes', async () => {
+    wrapper = createWrapper({
+      props: {
+        variant: 'outlined',
+      },
+    });
+
+    expectToHaveClass(wrapper.element, /_outlined_/);
+
+    await wrapper.setProps({ variant: 'flat' });
+    expectNotToHaveClass(wrapper.element, /_outlined_/);
+  });
+
+  it('should apply rounded classes', async () => {
+    wrapper = createWrapper({
+      props: {
+        rounded: 'md',
+      },
+    });
+
+    expectToHaveClass(wrapper.element, /_rounded__md_/);
+
+    await wrapper.setProps({ rounded: 'sm' });
+    expectToHaveClass(wrapper.element, /_rounded__sm_/);
+    expectNotToHaveClass(wrapper.element, /_rounded__md_/);
+
+    await wrapper.setProps({ rounded: 'lg' });
+    expectToHaveClass(wrapper.element, /_rounded__lg_/);
+  });
+
+  it('should apply dense class', async () => {
+    wrapper = createWrapper({
+      props: {
+        dense: false,
+      },
+    });
+
+    expectNotToHaveClass(wrapper.element, /_dense_/);
+
+    await wrapper.setProps({ dense: true });
+    expectToHaveClass(wrapper.element, /_dense_/);
+  });
+
+  it('should apply divide class', async () => {
+    wrapper = createWrapper({
+      props: {
+        divide: false,
+      },
+    });
+
+    expectNotToHaveClass(wrapper.element, /_divide_/);
+
+    await wrapper.setProps({ divide: true });
+    expectToHaveClass(wrapper.element, /_divide_/);
+  });
+
   it('should react to props changes', async () => {
     wrapper = createWrapper({
       props: {
